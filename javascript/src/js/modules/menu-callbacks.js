ebd05988fa49f726844cb6c3be61462e53491b5a
Daniel Junior <oidanieljunior@gmail.com>
2019-02-03 22:34:40 -0200

    Menu Callbacks to FetchGames

--- javascript/src/js/modules/menu-callbacks.js
+++ javascript/src/js/modules/menu-callbacks.js
@@ -3,22 +3,16 @@ function closeMenuContainer(menu) {
   menu.closeMenu();
 }
 
-export function anotherDate(menu) {
+export function anotherDate(menu, games) {
   closeMenuContainer(menu);
 }
 
-export function getNowGames(menu) {
+export function getNowGames(menu, games) {
   closeMenuContainer(menu);
-  const now = new Date();
+  games.hideOldGames();
+}
 
-  // Pega a hora atual e subtrai uma hora e meia
-  const timeToGames = Math.floor((now.getTime() - 5400000) / 1000);
-
-  const games = Array.from(document.querySelectorAll('.game'));
-
-  // Cria uma array com os jogos que já acabaram
-  // (com base apenas no tempo, ignorando possiveis atrasos)
-  const olders = games.filter(game => +game.dataset.startDate < timeToGames);
-
-  olders.forEach(older => older.classList.add('hidden'));
+export function showAllGames(menu, games) {
+  closeMenuContainer(menu);
+  games.showHiddenGames();
 }
